17102f79abd1ccda4b1c912b45ab79c886af7238
Dinh Phu <ndp17hp@gmail.com>
2020-10-18 23:44:51 +0700

    hoan thanh bai 38

--- src/main/java/com/supportportal/domain/HttpResponse.java
+++ src/main/java/com/supportportal/domain/HttpResponse.java
@@ -1,7 +1,13 @@
 package com.supportportal.domain;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
 
+import java.util.Date;
+
 public class HttpResponse {
+
+    @JsonFormat(shape =JsonFormat.Shape.STRING,pattern = "MM-dd-yyyy hh:mm:ss",timezone="Asia/Ho_Chi_Minh")
+    private Date timeStamp;
     private int httpStatusCode; // 200,201: success; ;400:user error ; 500 :server error
     private HttpStatus httpStatus;
     private String reason; // reason of http status
@@ -11,10 +17,19 @@ public class HttpResponse {
     }
 
     public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp=new Date();
         this.httpStatusCode = httpStatusCode;
         this.httpStatus = httpStatus;
         this.reason = reason;
         this.message = message;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
     }
 
     public int getHttpStatusCode() {
