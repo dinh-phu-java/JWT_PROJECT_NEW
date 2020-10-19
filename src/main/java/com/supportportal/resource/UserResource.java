44feba4de116f5d5b83fc9d92518934595c383f0
Dinh Phu <ndp17hp@gmail.com>
2020-10-19 09:58:44 +0700

    xong bai 43

--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -2,19 +2,20 @@ package com.supportportal.resource;
 
 
 import com.supportportal.domain.User;
+import com.supportportal.exception.domain.EmailExistException;
+import com.supportportal.exception.domain.EmailNotFoundException;
 import com.supportportal.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/user")
+@RequestMapping(path={"/","/user"})
 public class UserResource extends ExceptionHandling {
 
-
-
     @GetMapping("/home")
-    public String showUser(){
-        return "application works";
+    public String showUser() throws EmailExistException {
+//        return "application works";
+        throw new EmailExistException("This email address is already taken");
     }
 }
